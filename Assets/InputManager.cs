3f20cb6c3cbd6298cf47d4dbe6aba364d2e42790
lol <lol@lol.com>
2018-03-03 19:59:48 +0400

    added channel change feature for the radio
    control is based on rotation, , need to just add the actual music files and stereo sound

---
+++ Assets/InputManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	//This script should be attached to each controller (Controller Left or Controller Right)
+
+	// Getting a reference to the controller GameObject
+	private SteamVR_TrackedObject trackedObj;
+	// Getting a reference to the controller Interface
+	private SteamVR_Controller.Device Controller;
+	float RaycastHitDistance;
+	public LayerMask carRadioLayerMask;
+	public bool isSeeingRadio = false;
+	private bool isPointingAtRadio = false;
+	float startingAngle = 0.0f;
+	bool isChangingChannel = false;
+	public float currentStationAngle = 0.0f;
+	float newChannelAngle = 0.0f;
+	public GameObject Radio;
+
+	void Awake()
+	{
+		
+		// initialize the trackedObj to the component of the controller to which the script is attached
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+		RaycastHitDistance = 20.0f;
+		carRadioLayerMask = LayerMask.GetMask ("carRadioLayerMask");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+		Raycasting ();
+
+
+		Controller = SteamVR_Controller.Input((int)trackedObj.index);
+
+
+
+		// Getting the Trigger press
+		if (isPointingAtRadio && Controller.GetHairTriggerDown())
+		{
+			//Debug.Log ("initial");
+			//Debug.Log (Controller.transform.rot.eulerAngles.z);
+			startingAngle = Controller.transform.rot.eulerAngles.z;
+			isChangingChannel = true;
+		
+		}
+
+		if (isChangingChannel) {
+
+			//Debug.Log ("displacement");
+
+			float displacement = (startingAngle - Controller.transform.rot.eulerAngles.z) > 0 ? (startingAngle - Controller.transform.rot.eulerAngles.z): 360.0f + (startingAngle - Controller.transform.rot.eulerAngles.z);
+			adjustRadioChannel (displacement); 	
+
+		}
+
+
+
+		// Getting the Trigger Release
+		if (Controller.GetHairTriggerUp())
+		{
+			Debug.Log(gameObject.name + " Trigger Release");
+			isChangingChannel = false;
+			currentStationAngle = newChannelAngle;
+		}
+
+
+	}
+
+
+
+	void Raycasting(){
+		
+		
+		Vector3 fwd = transform.TransformDirection (Vector3.forward); //what is the direction in front of us
+		RaycastHit hit = new RaycastHit ();
+
+		if (Physics.Raycast (transform.position, fwd, out hit, RaycastHitDistance, carRadioLayerMask)) {
+		
+			isPointingAtRadio = true;
+	
+		} else {
+
+			isPointingAtRadio = false;
+
+		}
+	}
+
+
+	void adjustRadioChannel(float displacementValue){
+
+
+		newChannelAngle = (currentStationAngle + displacementValue * 0.5f) % 180.0f ;
+		//currentStationAngle = newChannelAngle;
+
+		//update the value of playSong prefab by some value.
+		Radio.GetComponent<audioScripts>().channelValue = newChannelAngle;
+
+
+	}
+
+
+}
---
+++ Assets/audioScripts.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioScripts : MonoBehaviour {
+
+	public int numChannels = 5;
+
+	public float channelValue = 0.0f;
+	public float basicVolume = 0.0f;
+	const float MAXVOLUME = 1.0f;
+	const float MAXSTEREOVOLUME = 1.0f;
+
+
+	public AudioClip stereoSound;
+	public Object[] songs;
+	int numSongs;
+	private bool[] songHasPlayed;
+	private AudioSource[] musicChannels;
+	private AudioSource stereoChannel;
+
+	// Use this for initialization
+	void Start () {
+
+	
+
+		
+	}
+
+	void Awake(){
+	
+		//load audio clips of mp3 files from "Music" folder
+		songs = Resources.LoadAll ("Music", typeof(AudioClip));
+		numSongs = songs.Length;
+
+	
+		//initialize an array that indicates whether a certain song has been played yet
+		songHasPlayed = new bool[numSongs];
+		for (int i = 0; i < numSongs; i++) {
+			songHasPlayed [i] = false;
+		}
+			
+		//generate music channels( that are of audio source types)
+		musicChannels = new AudioSource[numChannels];
+
+		//initialize channels
+		for (int i = 0; i < numChannels; i++) {
+			AudioSource channel = gameObject.AddComponent<AudioSource>();
+			//set the initial song this channel will be playing
+			channel.clip = songs[pickOneRandomSong()] as AudioClip;
+			channel.playOnAwake = true;
+			channel.loop = false;
+			channel.volume = i==0 ? 1.0f : 0.0f;
+			musicChannels [i] = channel;
+
+		}
+
+		//initialize stereo sound
+
+		stereoChannel = gameObject.AddComponent<AudioSource>();
+		stereoChannel.clip = stereoSound;
+		stereoChannel.playOnAwake = true;
+		stereoChannel.loop = true;
+		stereoChannel.volume = 0.0f;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		adjustRadioChannel ();
+		playNextSong ();
+
+	}
+
+
+	//if the station's music has stopped, play the next song
+	void playNextSong(){
+
+		for (int i = 0; i < numChannels; i++) {
+		
+			if (!musicChannels [i].isPlaying) {
+			
+				playRandomNewSong (i);
+			}
+
+		}
+	
+	}
+
+	void playRandomNewSong(int channelNumber){
+
+		musicChannels[channelNumber].clip = songs[pickOneRandomSong()] as AudioClip;
+		musicChannels [channelNumber].Play ();
+	
+	}
+
+	int pickOneRandomSong(){
+	
+		while ( true ) {
+			int randInt = Random.Range (0, songs.Length);
+
+			if (!songHasPlayed [randInt]) {
+				songHasPlayed [randInt] = true;
+				return randInt;
+			}
+		}
+	
+	}
+
+
+	void adjustRadioChannel(){
+
+	
+		//loop through channels to set volume for each channel
+		for (int i = 0; i < numChannels; i++) {
+
+			//calculate the degree discrepancy for this channel and that of the current channel value.
+			//This will be used not only as an indicator for determining the volume, but also whether the sound plays at all.
+
+			float channelChecker = Mathf.Abs( channelValue - (180.0f * i / (float) numChannels ) ) ;
+
+			//this channel is played
+			if (channelChecker <= 90.0f / (float)numChannels) {
+			
+				float changedVolumeRaw = (90.0f / (float)numChannels) - channelChecker;
+				float changedVolume = Mathf.Lerp ( 0.0f, MAXVOLUME, 1.0f - ((channelChecker) * (float)numChannels) / 90.0f);
+
+				changeChannelVolume (i, changedVolume + basicVolume);
+				
+
+			} else {
+				//this code may be redundant, but i'm not too sure how linear the transformation between channels will be.
+			
+					changeChannelVolume (i, 0);
+
+			}
+
+
+
+		}
+
+		//change the volume of the stereo background sound. It's based on sign function.
+
+		float stereoVolumeRaw = Mathf.Sin (2.0f * (float)numChannels * (channelValue - (180.0f * (float)numChannels))) + 1.0f; 
+		float stereoVolume = Mathf.Lerp ( 0.0f, MAXSTEREOVOLUME , stereoVolumeRaw / 2.0f);
+
+		changeStereoVolume (stereoVolume);
+
+
+	}
+
+
+	void changeChannelVolume(int i, float volume){
+		musicChannels [i].volume = volume;
+//		Debug.Log ("channel # " + i + " volume is: "+volume);
+
+	}
+
+	void changeStereoVolume(float volume){
+		stereoChannel.volume = volume;	
+//		Debug.Log ("stereo sound volume: "+ volume);
+	}
+
+}
